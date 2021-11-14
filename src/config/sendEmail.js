8f336346275fd66ba72f1d25bc885d575103a94a
Jitin Keshkar <cse.mkamble@gmail.com>
2021-11-14 15:39:24 +0530

    router created

---
+++ src/config/sendEmail.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const nodemailer = require('nodemailer');
+
+const sendEmail = (options) => {
+
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'cse.mkamble@gmail.com',
+            pass: 'Mayur#2000',
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.APP_NAME + ' <cse.mkamble@gmail.com>',
+        to: options.to,
+        subject: options.subject,
+        html: options.text,
+    }
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            // console.log(err)
+        } else {
+            // console.log(info)
+        }
+    })
+}
+
+module.exports = sendEmail;
