35ddfa1d11c00ff670884ecaccaeb8728f89903e
cse-mkamble <cse.mkamble@gmail.com>
2021-11-19 18:23:37 +0530

    alert add collapse

--- client/src/components/alert/Toast.js
+++ client/src/components/alert/Toast.js
@@ -3,23 +3,35 @@ import React from 'react';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import Stack from '@mui/material/Stack';
+import Collapse from '@mui/material/Collapse';
 
 const Toast = ({ msg, handleShow, severityType }) => {
+
+    const [open, setOpen] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setOpen(false);
+        }, 5000);
+    }, [open]);
+
     return (
-        <Stack sx={{ width: '100%' }} spacing={2}>
-            <div
-                style={{ top: '5px', right: '5px', minWidth: '300px', position: 'fixed', zIndex: 50 }}
-            >
-                <Alert
-                    variant="filled"
-                    severity={severityType}
-                    onClose={handleShow}
+        <Collapse in={open}>
+            <Stack sx={{ width: '100%' }} spacing={2}>
+                <div
+                    style={{ top: '5px', right: '5px', minWidth: '300px', position: 'fixed', zIndex: 50 }}
                 >
-                    <AlertTitle>{msg.title}</AlertTitle>
-                    {msg.body}
-                </Alert>
-            </div>
-        </Stack>
+                    <Alert
+                        variant="filled"
+                        severity={severityType}
+                        onClose={handleShow}
+                    >
+                        <AlertTitle>{msg.title}</AlertTitle>
+                        {msg.body}
+                    </Alert>
+                </div>
+            </Stack>
+        </Collapse>
     );
 }
 
