81d40eb32cb9512337587601f680f26c01253543
cse-mkamble <cse.mkamble@gmail.com>
2021-11-27 14:29:29 +0530

    header update

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-import Status from '../components/home/Status'
-import Posts from '../components/home/Posts'
-import RightSideBar from '../components/home/RightSideBar'
+import Status from '../components/home/Status';
+import Posts from '../components/home/Posts';
+import RightSideBar from '../components/home/RightSideBar';
 
-import { useSelector } from 'react-redux'
-import LoadIcon from '../images/loading.gif'
+import { useSelector } from 'react-redux';
+import LoadIcon from '../images/loading.gif';
 
 
 let scroll = 0;
@@ -14,7 +14,7 @@ const Home = () => {
     const { homePosts } = useSelector(state => state)
 
     window.addEventListener('scroll', () => {
-        if(window.location.pathname === '/'){
+        if (window.location.pathname === '/') {
             scroll = window.pageYOffset
             return scroll;
         }
@@ -22,14 +22,14 @@ const Home = () => {
 
     useEffect(() => {
         setTimeout(() => {
-            window.scrollTo({top: scroll, behavior: 'smooth'})
+            window.scrollTo({ top: scroll, behavior: 'smooth' })
         }, 100)
-    },[])
+    }, [])
 
     return (
         <div className="home row mx-0">
             <div className="col-md-8">
-                {/* <Status /> */}
+                <Status />
 
                 {/* {
                     homePosts.loading 
@@ -38,11 +38,11 @@ const Home = () => {
                         ? <h2 className="text-center">No Post</h2>
                         : <Posts />
                 } */}
-                
+
             </div>
-            
+
             <div className="col-md-4">
-                {/* <RightSideBar /> */}
+                <RightSideBar />
             </div>
         </div>
     )
