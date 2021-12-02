41c494799f003c52791b0a54de0fe5bd53e48edf
it-mkamble <cse.mkamble@gmail.com>
2021-12-02 19:55:58 +0530

    update info in profile

--- client/src/components/profile/Info.js
+++ client/src/components/profile/Info.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom'
-import Avatar from '../Avatar'
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { Box, Avatar, Button, Typography } from '@mui/material';
+
+// import Avatar from '../Avatar'
 import EditProfile from './EditProfile'
 import FollowBtn from '../FollowBtn'
 import Followers from './Followers'
@@ -9,139 +12,156 @@ import Following from './Following'
 import { GLOBALTYPES } from '../../redux/actions/globalTypes'
 import { MESS_TYPES, getConversations } from '../../redux/actions/messageAction'
 
-const Info = ({ id, auth, profile, dispatch }) => {
-    const [userData, setUserData] = useState([])
-    const [onEdit, setOnEdit] = useState(false)
+export default function Info({ id, auth, profile, dispatch }) {
+    const [userData, setUserData] = useState([]);
+    const [onEdit, setOnEdit] = useState(false);
 
-    const _dispatch = useDispatch()
-    const history = useHistory()
+    const _dispatch = useDispatch();
+    const history = useHistory();
 
-    const [showFollowers, setShowFollowers] = useState(false)
-    const [showFollowing, setShowFollowing] = useState(false)
+    const [showFollowers, setShowFollowers] = useState(false);
+    const [showFollowing, setShowFollowing] = useState(false);
 
     useEffect(() => {
         if (id === auth.user._id) {
-            setUserData([auth.user])
+            setUserData([auth.user]);
         } else {
-            const newData = profile.users.filter(user => user._id === id)
-            setUserData(newData)
+            const newData = profile.users.filter(user => user._id === id);
+            setUserData(newData);
         }
-    }, [id, auth, dispatch, profile.users])
-
+    }, [id, auth, dispatch, profile.users]);
 
     useEffect(() => {
         if (showFollowers || showFollowing || onEdit) {
-            dispatch({ type: GLOBALTYPES.MODAL, payload: true })
+            dispatch({ type: GLOBALTYPES.MODAL, payload: true });
         } else {
-            dispatch({ type: GLOBALTYPES.MODAL, payload: false })
+            dispatch({ type: GLOBALTYPES.MODAL, payload: false });
         }
-    }, [showFollowers, showFollowing, onEdit, dispatch])
+    }, [showFollowers, showFollowing, onEdit, dispatch]);
 
     const handleAddUser = (user) => {
-        _dispatch({ type: MESS_TYPES.ADD_USER, payload: { ...user, text: '', media: [] } })
+        _dispatch({ type: MESS_TYPES.ADD_USER, payload: { ...user, text: '', media: [] } });
         // dispatch({ type: MESS_TYPES.CHECK_ONLINE_OFFLINE, payload: online })
-        return history.push(`/message/${user._id}`)
+        return history.push(`/message/${user._id}`);
     }
 
     const isActive = (user) => {
         if (id === user._id) return 'active';
-        return ''
+        return '';
     }
 
-    return (
-        <div className="info">
-            {
-                userData.map(user => (
-                    <div className="info_container" key={user._id}>
-                        <Avatar src={user.avatar} size="supper-avatar" />
-
-                        <div className="info_content">
-                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-                                <span>
-                                    <div key={user._id} className={`message_user ${isActive(user)}`}
-                                        onClick={() => handleAddUser(user)}>
-                                            <div style={{
-                                        borderRadius: '50%',
-                                        background: 'rgb(186 186 187)',
-                                        height: '40px',
-                                        width: '40px',
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        textDecoration: 'none'
-                                    }} >    
-                                        <span
-                                            class="material-icons"
-                                            style={{ cursor: 'pointer' }}>
-                                            near_me
-                                        </span>
-                                        </div>
-                                    </div>
-                                </span>
-
-                                <span>
-                                    {
-                                        user._id === auth.user._id
-                                            ? <button className="btn btn-outline-info"
-                                                onClick={() => setOnEdit(true)}>
-                                                Edit Profile
-                                            </button>
-
-                                            : <FollowBtn user={user} />
-                                    }
-                                </span>
-
-                            </div>
+    return (<Box>
+        {userData.map(user => (<Box key={user._id}>
+            <Box sx={{ display: 'flex', }}>
+                <Box sx={{ width: '120px', p: 1 }} >
+                    <Box sx={{ my: 2, display: 'flex', justifyContent: 'center' }}>
+                        <Avatar
+                            alt=""
+                            src={user.avatar}
+                            sx={{ height: 80, width: 80 }}
+                        ></Avatar>
+                    </Box>
+                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold', fontSize: '14px' }}>{user.fullname}</Typography>
+                </Box>
+                <Box sx={{ width: 'calc(100% - 120px)', pr: 1 }}>
+                    <Typography fullWidth component='div' sx={{ mt: 3, fontSize: '22px' }}>{user.username}</Typography>
+                    {user._id === auth.user._id
+                        ? <Button
+                            fullWidth
+                            size='small'
+                            variant="outlined"
+                            color='inherit'
+                            onClick={() => setOnEdit(true)}
+                            sx={{ textTransform: 'none' }}
+                        >Edit Profile</Button>
+                        : <FollowBtn user={user} />}
+                </Box>
+            </Box>
 
 
-                            <div className="info_content_title">
-                                <h2>{user.username}</h2>
-                            </div>
+            {/* <div className="info_content">
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}> */}
 
-                            <div className="follow_btn">
-                                <span className="mr-4" onClick={() => setShowFollowers(true)}>
-                                    {user.followers.length} Followers
-                                </span>
-                                <span className="ml-4" onClick={() => setShowFollowing(true)}>
-                                    {user.following.length} Following
+            {/* <span>
+                        <div key={user._id} className={`message_user ${isActive(user)}`}
+                            onClick={() => handleAddUser(user)}>
+                            <div style={{
+                                borderRadius: '50%',
+                                background: 'rgb(186 186 187)',
+                                height: '40px',
+                                width: '40px',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textDecoration: 'none'
+                            }} >
+                                <span
+                                    class="material-icons"
+                                    style={{ cursor: 'pointer' }}>
+                                    near_me
                                 </span>
                             </div>
-
-
-
-                            <h6>{user.fullname} <span className="text-danger">{user.mobile}</span></h6>
-                            <p className="m-0">{user.address}</p>
-                            <h6 className="m-0">{user.email}</h6>
-                            <a href={user.website} target="_blank" rel="noreferrer">
-                                {user.website}
-                            </a>
-                            <p>{user.story}</p>
                         </div>
+                    </span> */}
+            {/* 
+                    <span>
+                        {
+                            user._id === auth.user._id
+                                ? <button className="btn btn-outline-info"
+                                    onClick={() => setOnEdit(true)}>
+                                    Edit Profile
+                                </button>
 
-                        {
-                            onEdit && <EditProfile setOnEdit={setOnEdit} />
+                                : <FollowBtn user={user} />
                         }
+                    </span>
 
-                        {
-                            showFollowers &&
-                            <Followers
-                                users={user.followers}
-                                setShowFollowers={setShowFollowers}
-                            />
-                        }
-                        {
-                            showFollowing &&
-                            <Following
-                                users={user.following}
-                                setShowFollowing={setShowFollowing}
-                            />
-                        }
-                    </div>
-                ))
+                </div>
+
+
+                <div className="info_content_title">
+                    <h2>{user.username}</h2>
+                </div>
+
+                <div className="follow_btn">
+                    <span className="mr-4" onClick={() => setShowFollowers(true)}>
+                        {user.followers.length} Followers
+                    </span>
+                    <span className="ml-4" onClick={() => setShowFollowing(true)}>
+                        {user.following.length} Following
+                    </span>
+                </div>
+
+
+
+                <h6>{user.fullname} <span className="text-danger">{user.mobile}</span></h6>
+                <p className="m-0">{user.address}</p>
+                <h6 className="m-0">{user.email}</h6>
+                <a href={user.website} target="_blank" rel="noreferrer">
+                    {user.website}
+                </a>
+                <p>{user.story}</p>
+            </div>
+
+            {
+                onEdit && <EditProfile setOnEdit={setOnEdit} />
             }
-        </div>
-    )
-}
 
-export default Info
+            {
+                showFollowers &&
+                <Followers
+                    users={user.followers}
+                    setShowFollowers={setShowFollowers}
+                />
+            }
+            {
+                showFollowing &&
+                <Following
+                    users={user.following}
+                    setShowFollowing={setShowFollowing}
+                />
+            } */}
+
+        </Box>))}
+    </Box>);
+}
--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,55 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Box, CircularProgress } from '@mui/material';
 
 import Info from '../../components/profile/Info'
 import Posts from '../../components/profile/Posts'
 import Saved from '../../components/profile/Saved'
-
-import { useSelector, useDispatch } from 'react-redux'
-import LoadIcon from '../../images/loading.gif'
 import { getProfileUsers } from '../../redux/actions/profileAction'
-import { useParams } from 'react-router-dom'
 
-
-const Profile = () => {
-    const { profile, auth } = useSelector(state => state)
-    const dispatch = useDispatch()
-
-    const { id } = useParams()
-    const [saveTab, setSaveTab] = useState(false)
+export default function Profile() {
+    const { profile, auth } = useSelector(state => state);
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const [saveTab, setSaveTab] = useState(false);
 
     useEffect(() => {
-        if(profile.ids.every(item => item !== id)){
-            dispatch(getProfileUsers({id, auth}))
+        if (profile.ids.every(item => item !== id)) {
+            dispatch(getProfileUsers({ id, auth }));
         }
-    },[id, auth, dispatch, profile.ids])
+    }, [id, auth, dispatch, profile.ids]);
 
-    return (
-        <div className="profile">
-            
-            <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
-
-            {
-                auth.user._id === id &&
-                <div className="profile_tab">
-                    <button className={saveTab ? '' : 'active'} onClick={() => setSaveTab(false)}>Posts</button>
-                    <button className={saveTab ? 'active' : ''} onClick={() => setSaveTab(true)}>Saved</button>
-                </div>
-            }
-
-            {
-                profile.loading 
-                ? <img className="d-block mx-auto" src={LoadIcon} alt="loading" />
-                : <>
-                    {
-                        saveTab
-                        ? <Saved auth={auth} dispatch={dispatch} />
-                        : <Posts auth={auth} profile={profile} dispatch={dispatch} id={id} />
-                    }
-                </>
-            }
-            
-        </div>
-    )
+    return (<Box>
+        <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
+        {auth.user._id === id && <div className="profile_tab">
+            <button className={saveTab ? '' : 'active'} onClick={() => setSaveTab(false)}>Posts</button>
+            <button className={saveTab ? 'active' : ''} onClick={() => setSaveTab(true)}>Saved</button>
+        </div>}
+        {profile.loading ? <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <CircularProgress />
+        </Box> : <>
+            {saveTab ? <Saved auth={auth} dispatch={dispatch} />
+                : <Posts auth={auth} profile={profile} dispatch={dispatch} id={id} />}
+        </>}
+    </Box>);
 }
-
-export default Profile
