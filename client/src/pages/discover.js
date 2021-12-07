434800be3c291a1b72fdcd91f142e5f2ea67d4d7
it-mkamble <cse.mkamble@gmail.com>
2021-12-07 10:19:50 +0530

    discover update

--- client/src/pages/discover.js
+++ client/src/pages/discover.js
@@ -1,50 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getDiscoverPosts, DISCOVER_TYPES } from '../redux/actions/discoverAction'
-import LoadIcon from '../images/loading.gif'
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Box } from '@mui/material';
+import { getDiscoverPosts, DISCOVER_TYPES } from '../redux/actions/discoverAction';
+import LoadingRole from "../components/LoadingRole";
 import PostThumb from '../components/PostThumb'
 import LoadMoreBtn from '../components/LoadMoreBtn'
-import { getDataAPI} from '../utils/fetchData'
+import { getDataAPI } from '../utils/fetchData'
 
-const Discover = () => {
-    const { auth, discover } = useSelector(state => state)
-    const dispatch = useDispatch()
-
-    const [load, setLoad] = useState(false)
+export default function Discover() {
+    const { auth, discover } = useSelector(state => state);
+    const dispatch = useDispatch();
+    const [load, setLoad] = useState(false);
 
     useEffect(() => {
-        if(!discover.firstLoad){
-            dispatch(getDiscoverPosts(auth.token))
+        if (!discover.firstLoad) {
+            dispatch(getDiscoverPosts(auth.token));
         }
-    },[dispatch, auth.token, discover.firstLoad])
+    }, [dispatch, auth.token, discover.firstLoad])
 
     const handleLoadMore = async () => {
-        setLoad(true)
-        const res = await getDataAPI(`post_discover?num=${discover.page * 9}`, auth.token)
-        dispatch({type: DISCOVER_TYPES.UPDATE_POST, payload: res.data})
-        setLoad(false)
+        setLoad(true);
+        const res = await getDataAPI(`post_discover?num=${discover.page * 9}`, auth.token);
+        dispatch({ type: DISCOVER_TYPES.UPDATE_POST, payload: res.data });
+        setLoad(false);
     }
 
-    return (
-        <div>
-            {
-                discover.loading 
-                ? <img src={LoadIcon} alt="loading" className="d-block mx-auto my-4" />
-                : <PostThumb posts={discover.posts} result={discover.result} />
-            }
+    return (<Box sx={{ maxWidth: '500px', m: 'auto' }}>
+        {discover.loading ? <LoadingRole />
+            : <PostThumb posts={discover.posts} result={discover.result} />}
 
-            {
-                load && <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
-            }
+        {load && <LoadingRole />}
 
-            {
-                !discover.loading &&
-                <LoadMoreBtn result={discover.result} page={discover.page}
-                load={load} handleLoadMore={handleLoadMore} />
-            }
-            
-        </div>
-    )
+        {!discover.loading && <LoadMoreBtn
+            result={discover.result}
+            page={discover.page}
+            load={load}
+            handleLoadMore={handleLoadMore} />}
+    </Box>);
 }
-
-export default Discover
