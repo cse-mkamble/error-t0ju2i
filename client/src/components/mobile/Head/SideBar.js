ea7f6efd1f5ed945f00ba7893e1afa0004a5df95
unknown <cse.mkamble@gmail.com>
2022-01-16 15:51:55 -0800

    SideBar component

--- client/src/components/mobile/Head/SideBar.js
+++ client/src/components/mobile/Head/SideBar.js
@@ -1,9 +1,21 @@
 import * as React from 'react';
-import { Box, Drawer } from '@mui/material';
+import { useState } from 'react';
+import { Box, Drawer, Toolbar, IconButton, Avatar, Typography, Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const drawerWidth = '80%';
 
 export default function SideBar(props) {
+    const [mode, setMode] = useState(true);
+
+    const handleModeToggle = () => {
+        setMode(!mode);
+    };
+
     return (<Drawer
         variant="temporary"
         open={props.drawerOpen}
@@ -13,8 +25,63 @@ export default function SideBar(props) {
         }}
         sx={{ '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth } }}
     >
-        <Box>
-            Hello
+        <Toolbar>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ direction: 'rtl' }} >
+                {mode ? <IconButton color="default" aria-label="Light Mode"
+                    onClick={handleModeToggle} >
+                    <LightModeOutlinedIcon />
+                </IconButton> : <IconButton color="default" aria-label="Dark Mode"
+                    onClick={handleModeToggle} >
+                    <DarkModeIcon />
+                </IconButton>}
+            </Box>
+        </Toolbar>
+        <Box sx={{ px: 2 }} >
+            <Avatar src={props.auth.user.avatar} sx={{ width: 86, height: 86 }} />
+            <Typography variant='h5' >{props.auth.user.fullname}</Typography>
+            <Typography sx={{ fontSize: '14px' }} >{props.auth.user.username}</Typography>
         </Box>
+        <Box sx={{ pt: 1 }} />
+        <Divider />
+        <List>
+            <ListItem onClick={() => {
+                window.location.href = `/messenger`;
+            }}>
+                <ListItemIcon>
+                    <ChatBubbleIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Messenger" sx={{ color: '#757575' }} />
+            </ListItem>
+            <ListItem onClick={() => {
+                window.location.href = `/dating`;
+            }}>
+                <ListItemIcon>
+                    <FavoriteIcon color="secondary" />
+                </ListItemIcon>
+                <ListItemText primary="Dating" sx={{ color: '#757575' }} />
+            </ListItem>
+            <ListItem onClick={() => {
+                window.location.href = `/setting`;
+            }}>
+                <ListItemIcon>
+                    <SettingsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Setting" sx={{ color: '#757575' }} />
+            </ListItem>
+        </List>
+        <Divider />
+        <List>
+            <ListItem onClick={() => {
+                window.location.href = `/invite`;
+            }}>
+                <ListItemText primary="Invite" sx={{ color: '#757575' }} />
+            </ListItem>
+            <ListItem onClick={() => {
+                window.location.href = `/appfeatures`;
+            }}>
+                <ListItemText primary="App Features" sx={{ color: '#757575' }} />
+            </ListItem>
+        </List>
     </Drawer>);
 }
--- client/src/components/mobile/Head/index.js
+++ client/src/components/mobile/Head/index.js
@@ -19,6 +19,7 @@ export default function Head() {
         <TopBar handleDrawerToggle={handleDrawerToggle} />
         <BottomBar auth={auth} currentTab={currentTab} />
         <SideBar
+            auth={auth}
             drawerOpen={drawerOpen}
             handleDrawerToggle={handleDrawerToggle}
         />
