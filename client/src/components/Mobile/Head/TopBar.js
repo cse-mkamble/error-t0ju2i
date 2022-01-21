0129b28017f3513751aeb6ed342b6ea8765ab0da
unknown <cse.mkamble@gmail.com>
2022-01-21 04:18:45 -0800

    V5.2.0 error

--- client/src/components/Mobile/Head/TopBar.js
+++ client/src/components/Mobile/Head/TopBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Box, AppBar, Toolbar, IconButton, Typography, Badge } from '@mui/material';
--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,74 @@
-import React from 'react';
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import Peer from 'peerjs';
 
 import Alert from './components/Alert';
-import NotFound from './components/NotFound';
+
+import PageRender from './HOC/PageRender';
+import PrivateRouter from './HOC/PrivateRouter';
 
 import Home from './pages/Home';
 import Signin from './pages/Authentication/Signin';
 import Signup from './pages/Authentication/Signup';
 
+import { GLOBALTYPES } from './redux/actions/globalTypes';
+import { refreshToken } from './redux/actions/authAction';
+import { getPosts } from './redux/actions/postAction';
+import { getSuggestions } from './redux/actions/suggestionsAction';
+import { getNotifies } from './redux/actions/notifyAction';
 import DataProvider from './redux/store';
 
 function MainRoute() {
   const { auth } = useSelector(state => state);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    // document.addEventListener('contextmenu', (e) => {
+    //   e.preventDefault();
+    // });
+    dispatch(refreshToken());
+    const socket = io();
+    dispatch({ type: GLOBALTYPES.SOCKET, payload: socket });
+    return () => socket.close();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (auth.token) {
+      dispatch(getPosts(auth.token));
+      dispatch(getSuggestions(auth.token));
+      dispatch(getNotifies(auth.token));
+    }
+  }, [dispatch, auth.token]);
+
+  useEffect(() => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    else if (Notification.permission === "granted") { }
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") { }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const newPeer = new Peer(undefined, {
+      path: '/', secure: true
+    });
+    dispatch({ type: GLOBALTYPES.PEER, payload: newPeer });
+  }, [dispatch]);
+
   return (<BrowserRouter>
     <Alert />
-    <Routes>
-      <Route path="/" element={auth.token ? <Home /> : <Signin />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <Switch>
+      <Route exact path="/" component={auth.token ? Home : Signin} />
+      <Route exact path="/signup" component={Signup} />
+      <PrivateRouter exact path="/:page" component={PageRender} />
+      <PrivateRouter exact path="/:page/:id" component={PageRender} />
+    </Switch>
   </BrowserRouter>);
 }
 
